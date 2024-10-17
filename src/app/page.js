e26769ff1ef679e01ff7ccbdf85aa2f0b1e56b2e
Gerald-ux-ux <11geraldmoney@gmail.com>
2024-10-17 18:52:32 +0300

    feat: adding a logo carousel

--- src/app/page.js
+++ src/app/page.js
@@ -2,12 +2,14 @@
 import FontTest from "@/components/font-test";
 import Footer from "@/components/footer";
 import InfinityHero from "@/components/InfinityHero";
+import LogoCarousel from "@/components/logo-carousel";
 import ScrollBar from "@/components/scroll-bar";
 
 export default function Home() {
   return (
     <>
       <ScrollBar />
+      <LogoCarousel />
       <FontTest />
       <Footer />
     </>

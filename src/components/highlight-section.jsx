9e2d2b13b88cd3eb32fa92032e1accd84a6406c4
Gerald-ux-ux <11geraldmoney@gmail.com>
2024-10-22 18:17:19 +0300

    style: testimonials

---
+++ src/components/highlight-section.jsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+
+export const Highlight = ({ children, className }) => {
+  return (
+    <span
+      className={cn(
+        "font-bold bg-emerald-100 text-emerald-700 dark:bg-emerald-700/[0.2] dark:text-emerald-500 px-1 py-0.5",
+        className,
+      )}
+    >
+      {children}
+    </span>
+  );
+};
